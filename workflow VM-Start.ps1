e5082e296af06ee2ef47b95e630388bcf8614505
NakayamaKento <kento427@outlook.com>
2021-06-05 18:30:50 +0900

    add commetn

--- workflow VM-Start.ps1
+++ workflow VM-Start.ps1
@@ -7,6 +7,7 @@ workflow VM-Start
     $Pro_List = ("win10pro1909", "win10pro2004","win10pro20H2", "win10pro21H1")
     $Edu_List = ("win10edu1909", "win10edu2004","win10edu20H2", "win10edu21H1")
 
+    #log in Azure
     Connect-AzAccount -Identity
     if($edition -eq "pro"){
         ForEach -Parallel -ThrottleLimit 4 ($VM in $Pro_List)
