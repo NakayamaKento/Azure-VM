278f8c4d71a555f62ec4f7c60b8fca1ec7984c91
NakayamaKento <kento427@outlook.com>
2021-06-05 18:47:44 +0900

    generalization

--- workflow VM-Start.ps1
+++ workflow VM-Start.ps1
@@ -1,25 +1,25 @@
-workflow VM-Start
+workflow VM-Stop
 {
     Param(
         [string]$edition
    )
    #VM list
-    $Pro_List = ("win10pro1909", "win10pro2004","win10pro20H2", "win10pro21H1")
-    $Edu_List = ("win10edu1909", "win10edu2004","win10edu20H2", "win10edu21H1")
+    $vm_List01 = ("VM01", "VM02","VM03", "VM04")
+    $vm_List02 = ("VM11", "VM12","VM13", "VM14")
 
     #log in Azure
     Connect-AzAccount -Identity
-    if($edition -eq "pro"){
+    if($edition -eq "list01"){
         ForEach -Parallel -ThrottleLimit 4 ($VM in $Pro_List)
         {
-            Start-AzVM -Name $VM -ResourceGroupName "win10update"
-            Write-Output "$VM Start."
+            Start-AzVM -ResourceGroupName "resourcegroup" -Name $VM -Force
+            Write-Output "$VM Stop."
         }
-    }elseif($edition -eq "edu"){
+    }elseif($edition -eq "list02"){
         ForEach -Parallel -ThrottleLimit 4 ($VM in $Edu_List)
         {
-            Start-AzVM -Name $VM -ResourceGroupName "win10update"
-            Write-Output "$VM Start."
+            Start-AzVM -ResourceGroupName "resourcegroup" -Name $VM -Force
+            Write-Output "$VM Stop."
         }
     }
 }
--- workflow VM-Stop.ps1
+++ workflow VM-Stop.ps1
@@ -3,20 +3,22 @@ workflow VM-Stop
     Param(
         [string]$edition
    )
-    $Pro_List = ("win10pro1909", "win10pro2004","win10pro20H2", "win10pro21H1")
-    $Edu_List = ("win10edu1909", "win10edu2004","win10edu20H2", "win10edu21H1")
+   #VM list
+    $vm_List01 = ("VM01", "VM02","VM03", "VM04")
+    $vm_List02 = ("VM11", "VM12","VM13", "VM14")
 
+    #log in Azure
     Connect-AzAccount -Identity
-    if($edition -eq "pro"){
+    if($edition -eq "list01"){
         ForEach -Parallel -ThrottleLimit 4 ($VM in $Pro_List)
         {
-            Stop-AzVM -ResourceGroupName "win10update" -Name $VM -Force
+            Stop-AzVM -ResourceGroupName "resourcegroup" -Name $VM -Force
             Write-Output "$VM Stop."
         }
-    }elseif($edition -eq "edu"){
+    }elseif($edition -eq "list02"){
         ForEach -Parallel -ThrottleLimit 4 ($VM in $Edu_List)
         {
-            Stop-AzVM -ResourceGroupName "win10update" -Name $VM -Force
+            Stop-AzVM -ResourceGroupName "resourcegroup" -Name $VM -Force
             Write-Output "$VM Stop."
         }
     }
