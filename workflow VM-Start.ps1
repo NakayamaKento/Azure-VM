664dea380e6c84247650d9a51e6d1d01ee2ade7a
NakayamaKento <kento427@outlook.com>
2021-06-05 18:25:00 +0900

    initial commit

---
+++ workflow VM-Start.ps1
@@ -0,0 +1,23 @@
+workflow VM-Start
+{
+    Param(
+        [string]$edition
+   )
+    $Pro_List = ("win10pro1909", "win10pro2004","win10pro20H2", "win10pro21H1")
+    $Edu_List = ("win10edu1909", "win10edu2004","win10edu20H2", "win10edu21H1")
+
+    Connect-AzAccount -Identity
+    if($edition -eq "pro"){
+        ForEach -Parallel -ThrottleLimit 4 ($VM in $Pro_List)
+        {
+            Start-AzVM -Name $VM -ResourceGroupName "win10update"
+            Write-Output "$VM Start."
+        }
+    }elseif($edition -eq "edu"){
+        ForEach -Parallel -ThrottleLimit 4 ($VM in $Edu_List)
+        {
+            Start-AzVM -Name $VM -ResourceGroupName "win10update"
+            Write-Output "$VM Start."
+        }
+    }
+}
